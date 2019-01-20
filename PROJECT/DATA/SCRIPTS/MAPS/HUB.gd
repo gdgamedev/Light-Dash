b1dd60d4cfa6c45e51137b1a13e2aafb099c4b4c
gdgamedev <ghsoares99795@gmail.com>
2019-01-20 12:36:26 -0200

    deletando todos os arquivos para refazer

--- PROJECT/DATA/SCRIPTS/MAPS/HUB.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-#chamado quando iniciado
-func _ready():
-	$"GUI/TRANSITION/ANIM".play("fade_out")
-	pass
-
-
-
--- PROJECT/DATA/SCRIPTS/SPLASH_SCENE.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-
-func _on_ANIM_animation_finished(anim_name):
-	get_tree().change_scene("res://DATA/SCENES/MISC/MAIN_MENU.tscn")
-	pass # replace with function body
